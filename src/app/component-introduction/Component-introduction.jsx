24469dbf5361373cf7caae92d28437634e0c6ecf
Fabio Tomas Naene <fabiotomasnaene@gmail.com>
2026-04-03 17:21:41 +0200

    criação do componente introduction

---
+++ src/app/component-introduction/Component-introduction.jsx
@@ -0,0 +1,16 @@
+import styles from './Component-introduction.module.css';
+import stylesComponentHeader from '../component-header/ComponentHeader.module.css';
+import logo from '../../../public/logo.jpeg';
+import Image from 'next/image';
+
+export default function ComponentIntroduction({theme}) {
+    return (
+        <article className={styles.containerIntrodunction}>
+            <div className={styles.containerTitle}>
+                <Image src={logo} alt="Logo" className={theme ? stylesComponentHeader.iconInvert : ''} width={50} height={50}/>
+                <h1 className={styles.title}>Orgamize seu conhecimento</h1>
+            </div>
+            <p className={styles.description}>Guarde, organize e acesse seu conhecimento de forma simples e eficiente.</p>
+        </article>
+    )
+}
