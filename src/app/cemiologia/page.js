31c16980ddecb481381c115308c96b6168b9b12c
Fabio Tomas Naene <fabiotomasnaene@gmail.com>
2026-04-26 08:31:00 +0200

    CEMIOLOGIA

--- src/app/cemiologia/page.js
+++ src/app/cemiologia/page.js
@@ -11,7 +11,7 @@ export default function Cemiologia() {
     <main
       className={`${styles.main} ${theme ? styleTheme.dark : styleTheme.light}`}
     >
-      <h1 className={styles.titlePage}>História clínica do utente</h1>
+      <h1 className={styles.titlePage}>CEMIOLOGIA</h1>
       <div className={styles.container}>
         <div className={styles.containerDisease}>
           <div>
@@ -26,7 +26,7 @@ export default function Cemiologia() {
               >
                 ➡️
               </span>
-              Introdução a História clínica do utente - conceitos fundamentais
+              Introdução a Cemiologia - conceitos fundamentais
             </button>
 
             <div
@@ -36,6 +36,82 @@ export default function Cemiologia() {
                   : styles.contentClosed
               }
             >
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Cemiologia  </span>
+                é uma área da medicina que estuda os sinais e sintomas das patologias, com o objetivo de ajudar na identificação de diagnóstico
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Sinal  </span>
+                é o que o profissional observa.
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Sintoma  </span>
+                é o que o paciente sente e diz.
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Propadiotismo/propadiótica  </span>
+                é o conjunto de conhecimentos e técnicas usadas para examinar o paciente com o objetivo de identificar sinais de patologias, confirmar informações de anamnese.
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>  </span>
+                Inclui métodos básicos de exame físico como:
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>anamnese  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Inspeção  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Palpação  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Percusão  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Auscutação  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Cemiotécnica  </span>
+                é a parte prática da Cemiologia ou conjunto de técnicas usadas para examinar o paciente e identificar sinais e sintomas. Com o objetivo de aplicar corretamente o exame físico, detectar sinais, conpletar exames e ajudar no diagnóstico.
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>  </span>
+                A Cemiotécnica involve:
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>anamnese  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Inspeção  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Palpação  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Percusão  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>Auscutação  </span>
+
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>EX:  </span>
+                Um paciente com dor no peito: o profissional usa Auscutação para ouvir o coração, e isso é Cemiologia em ação
+              </p>
+              <p className={styles.paragraph}>
+                <span className={styles.highlight}>História clínica  </span>
+                é o registo completo organizado de todas as informações sobre o paciente, recolhidas durante o atendimento. Com o objetivo de ajudar diagnóstico, tratamento e atendimento.
+              </p>
             </div>
           </div>
         </div>
